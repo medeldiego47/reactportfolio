0eeec9e7556791c6b1293fdadf8404549a634a95
DiegoM <medeldiego47@yahoo.com>
2023-01-06 20:55:34 -0600

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,13 @@ export default class Contact extends Component {
   render() {
     return (
       <section className='m-4 contactHeading' id='Contact'>
-        <h4>Get in Touch</h4>
+        <h2>Contact Me</h2>
         <hr />
         <div className='container contact_container pt-'>
             <form action=''>
-                <input type='text' name='name' placeholder='Your Name' required />
-                <input type='email' name='email' placeholder='email@email.com' required />
-                <textarea name='message' rows='7' placeholder='Your Message Here' required></textarea>
+                <input type='text' name='name' placeholder='Name' required />
+                <input type='email' name='email' placeholder='Email' required />
+                <textarea name='message' rows='2' placeholder='Message' required></textarea>
                 <button type='submit' className='btn btnCustom'>Send Message</button>
             </form>
         </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <div> <section class="center">
+      <ul>
+          <li><a href="mailto: medeldiego47@yahoo.com">medeldiego47@yahoo.com</a></li>
+          <li><a href="https://github.com/medeldiego47" target="blank">Github</a></li>
+          <li><a Href="https://www.linkedin.com/in/diego-medel-9b01a8215/" target="blank">linked-in</a></li>
+      </ul>
+      
+  </section></div>
     )
   }
 }
