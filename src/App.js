debf4d88e14b363c08163f800297a5a9e67a062d
DiegoM <medeldiego47@yahoo.com>
2023-01-06 22:13:38 -0600

    build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Projects from './components/Projects';
+import Projects from './pages/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <main>
     <div className='App '>
       <Header />
-     
-      <About />
+     <About />
       <Projects />
       <Contact />
       <Footer />
     </div>
+    </main>
   )
 }
 
---
+++ src/pages/Projects.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import bored from '../images/bored.png'
+import jobb from '../images/jobb.png'
+import employee from '../images/employee.png'
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>Projects
+        <section id="Project">
+            <h2 class="container">Portfolio</h2>
+        <div class="container3">
+            <div class="container3">
+                <a href="https://github.com/medeldiego47/employee_tracker" target="blank"><img class="img" src={employee} width="200px" height="200px" alt="project 1"></img></a>
+                <p class="cap">employee tracker</p>
+                
+            </div>
+            <div class="container3">
+                <a href="https://catpolk.github.io/Bored-NO-MO-project-1/"><img src={bored} width="200px" height="200px" alt="project 1"></img></a>
+                <p class="cap">Bored No Mo</p>
+                
+            </div>
+            <div class="container3">
+                <a href="https://grabajob.herokuapp.com/"><img src={jobb} width="200px" height="200px" alt="project 2"></img></a>
+
+                <p class="cap">GrabAJob</p>
+            </div>
+           
+          
+        </div>
+    </section>
+      </div>
+    )
+  }
+}
+
